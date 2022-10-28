bf81231e5cec700ba58744eb1648ab507880238c
Wallace07 <william_gra@hotmail.com>
2022-10-27 20:41:31 -0500

    5 actualización

--- src/BookList.js
+++ src/BookList.js
@@ -6,7 +6,7 @@ function Lista () {
     return <div className="Lista">
         <Book title="Alicia en el pais de las maravillas"></Book>
         <Book title="Sapienes de animales a dioses"></Book>
-        <Book title="Don quijote de la mancha"></Book>
+        <Book title="Don quijote de la mancha "></Book>
     </div>
 }
 
